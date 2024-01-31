242a12ac19b00597dfd0aaadd034633f9dbd1324
Katarina Ogliara <katarina_ogliara@hotmail.com>
2024-01-31 15:09:19 +0100

    git commit --all

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -1,5 +1,10 @@
+using System;
+
 class math{
 	static void Main(){
-		System.Console.Write("Hi\n");
+		double x =(3d/2d) * Math.PI;
+		double sinx = Math.Sin(x);
+		double abs_sinx = Math.Abs(sinx);
+		System.Console.Write("Hi, sin to {0} is {1}, and the absolute value of {1} is {2}.\n", x, sinx, abs_sinx);
 	}
 }
