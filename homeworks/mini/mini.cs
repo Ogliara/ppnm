c7a353458b4ebd525da4590a76aaa4e490dd7f99
Katarina Ogliara <katarina_ogliara@hotmail.com>
2024-05-20 09:46:53 +0200

    Started next exercise.

---
+++ homeworks/mini/mini.cs
@@ -0,0 +1,10 @@
+using System;
+using static System.Math;
+using static System.Console;
+
+public static class mini{
+	//NOTE: Change 'void' to 'vector' later.
+	public static void newton(){
+		
+	}//newton class
+}//mini class
